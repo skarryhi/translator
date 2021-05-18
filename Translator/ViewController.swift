7e3509d44b744fb0c8201ac0eeebc0292a936d12
skarry <60601122@mail.ru>
2021-05-18 20:38:24 +0300

    Initial Commit

---
+++ Translator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Translator
+//
+//  Created by Saturn Karry on 5/18/21.
+//  Copyright © 2021 ru.skarry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
